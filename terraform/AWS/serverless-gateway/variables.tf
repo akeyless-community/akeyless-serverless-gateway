c42a22d32f1bd7ce34299deb3b0c2e98b000d31f
akeyless-ci-community <action@github.com>
2026-08-17 13:40:57 +0000

    Updated Serverless-Gateway version to latest: 5.2.0

--- terraform/AWS/serverless-gateway/variables.tf
+++ terraform/AWS/serverless-gateway/variables.tf
@@ -14,7 +14,7 @@ variable "region" {
 variable "image_tag" {
   description = "Image tag of the image from AWS Public ECR Image Gallery we are pulling from"
   type        = string
-  default     = "5.1.0"
+  default     = "5.2.0"
 }
 
 variable "create_log_group_for_lambda" {
